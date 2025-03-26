eda73cde2451e70c1dc48c2cac1dd9bfb9b81327
Iris Josiely <116114861+IrisJosiely@users.noreply.github.com>
2025-03-26 20:06:47 -0300

    Atualização dos arquivos HTML, CSS e JS

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ function toggleMenu() {
     var navLinks = document.getElementById('nav-links');
     navLinks.classList.toggle('active');
 }
+
+// Fecha o menu quando um link é clicado
+document.querySelectorAll('.nav-links a').forEach(link => {
+    link.addEventListener('click', () => {
+        document.getElementById('nav-links').classList.remove('active');
+    });
+});
+
